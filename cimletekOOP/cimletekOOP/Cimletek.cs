d73c5038be01b3714159245a1cb91f82e9e6f40a
M4L4MUT <113418446+M4L4MUT@users.noreply.github.com>
2023-03-23 09:57:46 +0100

    Add files via upload

---
+++ cimletekOOP/cimletekOOP/Cimletek.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CimletekOOP
+{
+    internal class Cimletek
+    {
+        private int osszeg;
+        private int[,] cimletek = { {20000, 0 } ,
+                                    {10000, 0 } ,
+                                    {5000, 0 } ,
+                                    {2000, 0},
+                                    {1000,0 },
+                                    {500,0 },
+                                    {200,0 },
+                                    {100,0 },
+                                    {50,0 },
+                                    {20,0 },
+                                    {10,0 },
+                                    {5,0 }, };
+       
+       public Cimletek() { }   
+       
+       public void setOsszeg(int p) { 
+        
+            this.osszeg= kerekit(p);
+       }
+
+        private int kerekit(int p)
+        {
+            if (p % 5 < 3)
+            {
+                p = p - p%5;
+            }
+            else p = p - p%5 + 5;
+
+            return p;
+        }
+
+        public Cimletek(int osszeg)
+        {
+            this.osszeg= kerekit(osszeg);
+
+            
+        }
+
+        public void Szamol()
+        {
+            for (int i = 0; i < this.cimletek.GetLength(0); i++)
+            {
+                if (this.osszeg >= this.cimletek[i,0])
+                {
+                    this.cimletek[i,1] = this.osszeg / cimletek[i, 0];
+                    this.osszeg = this.osszeg % this.cimletek[i, 0];
+                }
+            }
+        }
+        public int[,] getCimletek()
+        {
+            return cimletek;
+        }
+    }
+}
---
+++ cimletekOOP/cimletekOOP/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+
+namespace CimletekOOP
+{
+    internal class Cimlet
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Adja meg az összeget Forintban:");
+            int osszeg = int.Parse(Console.ReadLine());
+           
+            Cimletek c1 = new Cimletek();
+            c1.setOsszeg(osszeg);
+            c1.Szamol();
+            int[,] t = c1.getCimletek();
+            
+            for (int i = 0;i < t.GetLength(0); i++)
+            {
+                if (t[i, 1] != 0)
+                Console.WriteLine("{0} címletből {1} db kell", t[i, 0],t[i, 1]);
+            }
+            Console.ReadKey();
+        }+
+    }
+}
