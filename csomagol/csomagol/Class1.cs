3d87ba400116d71749d061e197d3ae9b16c304d3
M4L4MUT <113418446+M4L4MUT@users.noreply.github.com>
2023-03-24 07:49:50 +0100

    Add files via upload

---
+++ csomagol/csomagol/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csomagolOOP
+{
+    internal class Dinnye
+    {
+        private int darab;
+        private int atmero;
+        private double anyag;
+
+        public Dinnye() { }
+
+        public void setDarab(int d) { this.darab = d; }
+        public void setAtmero(int a) { this.atmero = a; }
+
+        public void Szamol()
+        {
+            this.anyag = this.darab * (this.atmero * Math.PI + 60);
+        }
+
+        public double getAnyag() { return this.anyag; }
+    }
+}
---
+++ csomagol/csomagol/dinnye.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csomagolOOP
+{
+    internal class Dinnye
+    {
+        private int darab;
+        private int atmero;
+        private int anyag;
+        private int sugar = this.atmero / 2;
+        private int pi = 3.14; 
+
+        public Dinnye() { }
+
+        public void setDarab(int d) { this.darab = d }
+        public void setAtmero(int a) { this.atmero = a }
+        public void setSugar(int r) { this.sugar = r }
+        public void setPI(int pi) { this.pi = pi }
+
+        public void Szamol() 
+        {
+            this.anyag = this.darab * 4 * this.sugar * this.sugar * this.pi + 60;
+        }
+
+        public int getAnyag() { return this.anyag; }
+    }
+}
