7ddda695a199b586cc5153a98e4f67c42c1569c2
M4L4MUT <113418446+M4L4MUT@users.noreply.github.com>
2023-03-26 18:13:43 +0200

    Add files via upload

---
+++ 03.26 masodfoku/Masodfoku/Masodfoku/Masodfoku.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masodfoku
+{
+    internal class Masodfoku
+    {
+
+        private double a, b, c, eredmeny1, eredmeny2, gyok;
+
+        public Masodfoku() { }
+
+        public void setA(double a) { this.a = a; }
+        public void setB(double b) { this.b = b; }
+        public void setC(double c) { this.c = c; }
+        public double getEredmeny1() { return this.eredmeny1; }
+        public double getEredmeny2() { return this.eredmeny2; }
+
+        public void Szamol()
+        {
+            this.eredmeny1 = (-this.b + Math.Sqrt((this.b * this.b) - (4 * this.a * this.c))) / (2 * this.a);
+            this.eredmeny2 = (-this.b - Math.Sqrt((this.b * this.b) - (4 * this.a * this.c))) / (2 * this.a);
+        }
+    }
+}
---
+++ 03.26 masodfoku/Masodfoku/Masodfoku/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masodfoku
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adja meg az első számot.");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg a második számot.");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg a harmadik számot.");
+            int c = int.Parse(Console.ReadLine());
+
+            Masodfoku c1 = new Masodfoku();
+            c1.setA(a);
+            c1.setB(b);
+            c1.setC(c);
+            c1.Szamol();
+            double e1 = c1.getEredmeny1();
+            double e2 = c1.getEredmeny2();
+
+
+            Console.WriteLine("Az első megoldás {0}, a második {1}",e1, e2);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
