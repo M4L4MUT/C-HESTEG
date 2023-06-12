8aad525fe834fb8352a143b9bd421744478d4211
M4L4MUT <113418446+M4L4MUT@users.noreply.github.com>
2023-06-12 13:28:45 +0200

    Add files via upload

---
+++ 18.6/18.6/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace _18._6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.papi1 = new System.Windows.Forms.PictureBox();
+            this.papi2 = new System.Windows.Forms.PictureBox();
+            this.papi3 = new System.Windows.Forms.PictureBox();
+            this.papi6 = new System.Windows.Forms.PictureBox();
+            this.papi4 = new System.Windows.Forms.PictureBox();
+            this.papi5 = new System.Windows.Forms.PictureBox();
+            this.papi8 = new System.Windows.Forms.PictureBox();
+            this.papi7 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.papi1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.papi2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.papi3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.papi6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.papi4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.papi5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.papi8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.papi7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // papi1
+            // 
+            this.papi1.Image = ((System.Drawing.Image)(resources.GetObject("papi1.Image")));
+            this.papi1.Location = new System.Drawing.Point(267, 12);
+            this.papi1.Name = "papi1";
+            this.papi1.Size = new System.Drawing.Size(95, 100);
+            this.papi1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.papi1.TabIndex = 0;
+            this.papi1.TabStop = false;
+            // 
+            // papi2
+            // 
+            this.papi2.Image = ((System.Drawing.Image)(resources.GetObject("papi2.Image")));
+            this.papi2.Location = new System.Drawing.Point(411, 12);
+            this.papi2.Name = "papi2";
+            this.papi2.Size = new System.Drawing.Size(95, 100);
+            this.papi2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.papi2.TabIndex = 1;
+            this.papi2.TabStop = false;
+            // 
+            // papi3
+            // 
+            this.papi3.Image = ((System.Drawing.Image)(resources.GetObject("papi3.Image")));
+            this.papi3.Location = new System.Drawing.Point(540, 125);
+            this.papi3.Name = "papi3";
+            this.papi3.Size = new System.Drawing.Size(95, 100);
+            this.papi3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.papi3.TabIndex = 2;
+            this.papi3.TabStop = false;
+            // 
+            // papi6
+            // 
+            this.papi6.Image = ((System.Drawing.Image)(resources.GetObject("papi6.Image")));
+            this.papi6.Location = new System.Drawing.Point(267, 352);
+            this.papi6.Name = "papi6";
+            this.papi6.Size = new System.Drawing.Size(95, 100);
+            this.papi6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.papi6.TabIndex = 3;
+            this.papi6.TabStop = false;
+            // 
+            // papi4
+            // 
+            this.papi4.Image = ((System.Drawing.Image)(resources.GetObject("papi4.Image")));
+            this.papi4.Location = new System.Drawing.Point(540, 272);
+            this.papi4.Name = "papi4";
+            this.papi4.Size = new System.Drawing.Size(95, 100);
+            this.papi4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.papi4.TabIndex = 4;
+            this.papi4.TabStop = false;
+            // 
+            // papi5
+            // 
+            this.papi5.Image = ((System.Drawing.Image)(resources.GetObject("papi5.Image")));
+            this.papi5.Location = new System.Drawing.Point(411, 352);
+            this.papi5.Name = "papi5";
+            this.papi5.Size = new System.Drawing.Size(95, 100);
+            this.papi5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.papi5.TabIndex = 5;
+            this.papi5.TabStop = false;
+            // 
+            // papi8
+            // 
+            this.papi8.Image = ((System.Drawing.Image)(resources.GetObject("papi8.Image")));
+            this.papi8.Location = new System.Drawing.Point(139, 125);
+            this.papi8.Name = "papi8";
+            this.papi8.Size = new System.Drawing.Size(95, 100);
+            this.papi8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.papi8.TabIndex = 6;
+            this.papi8.TabStop = false;
+            // 
+            // papi7
+            // 
+            this.papi7.Image = ((System.Drawing.Image)(resources.GetObject("papi7.Image")));
+            this.papi7.Location = new System.Drawing.Point(139, 272);
+            this.papi7.Name = "papi7";
+            this.papi7.Size = new System.Drawing.Size(95, 100);
+            this.papi7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.papi7.TabIndex = 7;
+            this.papi7.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 61);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Balra";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(411, 209);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 61);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Jobbra";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.papi7);
+            this.Controls.Add(this.papi8);
+            this.Controls.Add(this.papi5);
+            this.Controls.Add(this.papi4);
+            this.Controls.Add(this.papi6);
+            this.Controls.Add(this.papi3);
+            this.Controls.Add(this.papi2);
+            this.Controls.Add(this.papi1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.papi1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.papi2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.papi3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.papi6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.papi4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.papi5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.papi8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.papi7)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox papi1;
+        private System.Windows.Forms.PictureBox papi2;
+        private System.Windows.Forms.PictureBox papi3;
+        private System.Windows.Forms.PictureBox papi6;
+        private System.Windows.Forms.PictureBox papi4;
+        private System.Windows.Forms.PictureBox papi5;
+        private System.Windows.Forms.PictureBox papi8;
+        private System.Windows.Forms.PictureBox papi7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 18.6/18.6/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _18._6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image s = papi1.Image;
+            papi1.Image = papi2.Image;
+            papi2.Image = papi3.Image;
+            papi3.Image = papi4.Image;
+            papi4.Image = papi5.Image;
+            papi5.Image = papi6.Image;
+            papi6.Image = papi7.Image;
+            papi7.Image = papi8.Image;
+            papi8.Image = s;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Image s = papi8.Image;
+            papi8.Image = papi7.Image;
+            papi7.Image = papi6.Image;
+            papi6.Image = papi5.Image;
+            papi5.Image = papi4.Image;
+            papi4.Image = papi3.Image;
+            papi3.Image = papi2.Image;
+            papi2.Image = papi1.Image;
+            papi1.Image = s;
+        }
+    }
+}
